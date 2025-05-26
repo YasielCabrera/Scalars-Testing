05b6405d35aad40a90d470dfcf48a30ee43db9ef
alejandrocabriales <alejandro.cabriales@dspot.com.pl>
2025-05-26 14:44:07 -0300

    fix(scalars): remove import from index

--- index.ts
+++ index.ts
@@ -2,7 +2,6 @@ import { type Manifest } from "document-model";
 import manifestJson from "./powerhouse.manifest.json" with { type: "json" };
 import * as documentModelsExports from "./document-models/index.js";
 import * as editorsExports from "./editors/index.js";
-import "@powerhousedao/document-engineering/dist/style.css";
 
 export const manifest: Manifest = manifestJson;
 export const documentModels = Object.values(documentModelsExports);
